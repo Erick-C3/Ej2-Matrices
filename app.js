541d776d37fca9e0149a8372c5165af0f4f0620a
Erick <erick.nahuelpaiva.001@gmail.com>
2023-09-05 09:43:19 -0300

    ej 2 v1 terminado

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
 const divTablero = document.querySelector("#tablero");
+const inputFila = document.querySelector("#fila");
+const inputCol = document.querySelector("#col");
 
-const MAX_FILA = 5;
-const MAX_COL = 6;
+/* const MAX_FILA = 3;
+const MAX_COL = 5; */
 
-for (let fila = 0; fila < MAX_FILA; fila++) {
-    /* matriz.push([])    */ 
-    divTablero.innerHTML += `
-        <div class="row">
-            ${ agregarColumnas() }
-        </div>
-    `
+
+
+function generarMatriz() {
+    let maxFila = parseInt(inputFila.value);
+
+    divTablero.innerHTML = "";    
+    for (let fila = 0; fila < maxFila; fila++) {
+        /* matriz.push([])    */ 
+        divTablero.innerHTML += `
+            <div class="row">
+                ${ agregarColumnas() }
+            </div>
+        `
+    }
 }
 
 function agregarColumnas() {
+    let maxFila = parseInt(inputCol.value);
     let columnas = "";
-    for (let columna = 0; columna < MAX_COL; columna++) {
+    for (let columna = 0; columna < maxFila; columna++) {
         columnas += `
             <div class="col casilla"> </div>
         `;
     }
     return columnas;
 }
+
