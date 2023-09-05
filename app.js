fd63b82ececd285c77f645596742199e2d89b0a5
Erick <erick.nahuelpaiva.001@gmail.com>
2023-09-05 09:29:49 -0300

    maquetado generado con js

---
+++ app.js
@@ -0,0 +1,23 @@
+const divTablero = document.querySelector("#tablero");
+
+const MAX_FILA = 5;
+const MAX_COL = 6;
+
+for (let fila = 0; fila < MAX_FILA; fila++) {
+    /* matriz.push([])    */ 
+    divTablero.innerHTML += `
+        <div class="row">
+            ${ agregarColumnas() }
+        </div>
+    `
+}
+
+function agregarColumnas() {
+    let columnas = "";
+    for (let columna = 0; columna < MAX_COL; columna++) {
+        columnas += `
+            <div class="col casilla"> </div>
+        `;
+    }
+    return columnas;
+}
